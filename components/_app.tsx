35d466aa8447cb394b98dba40ec70888055b6247
QuantumForger <quantumaiforge@gmail.com>
2024-12-10 13:10:25 +0000

    app.tsx 1

--- components/_app.tsx
+++ components/_app.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
+import './index.css'; // Global CSS styles
 
+// Access the root element in the DOM
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+// Render the React application
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
