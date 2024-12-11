5e578f16db1f056c34bc6076353d076ec0195361
QuantumForger <quantumaiforge@gmail.com>
2024-12-11 10:08:11 +0000

    ,kk

--- components/_app.tsx
+++ components/_app.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
+import App from './App'; // Ensure App is defined in './App.tsx' or './App.jsx'
 import './index.css'; // Global CSS styles
 
 // Access the root element in the DOM
