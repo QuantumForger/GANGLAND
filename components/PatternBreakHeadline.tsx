f2d690f62cc99645d547e715bce5931a8260af45
QuantumForger <quantumaiforge@gmail.com>
2024-12-11 23:09:31 +0000

    again a

--- components/PatternBreakHeadline.tsx
+++ components/PatternBreakHeadline.tsx
@@ -1,9 +1,11 @@
+'use client'; // Add this line to make it a Client Component
+
 import { useEffect } from 'react';
 
-const PatternBreakHeadline = () => {
+const PatternBreakHeadlineClient = () => {
   useEffect(() => {
     const gradientAnimation = `
-      /* Your gradient animation CSS here */
+      /* Add your gradient animation CSS here */
     `;
 
     const style = document.createElement('style');
@@ -14,12 +16,9 @@ const PatternBreakHeadline = () => {
       // Cleanup: Remove the style element on unmount
       document.head.removeChild(style);
     };
-  }, []); // Empty dependency array ensures it runs only once
+  }, []);
 
-  return (
-    <h1>Your headline here</h1>
-  );
+  return <h1>Your Headline Here</h1>;
 };
 
-export default PatternBreakHeadline;
-
+export default PatternBreakHeadlineClient;
